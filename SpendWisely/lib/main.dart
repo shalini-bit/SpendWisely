7f00ed683a007aab38890d595e3e1e95b0652a07
Shalini <64901722+shalini-bit@users.noreply.github.com>
2021-09-29 19:11:36 +0530

    First Commit

---
+++ SpendWisely/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: HomePage(),
+    ));
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Color primaryColor = Color(0xff0f284b);
+  Color secondaryColor = Color(0xff232c51);
+  Color logoPurple = Color(0xff6a1b9a);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: primaryColor,
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              'assets/SpendWizely.png',
+              height: 250,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            //Texts and Styling of them
+            Text(
+              'Welcome to SpendWisely !',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white, fontSize: 28),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'A one-stop portal for you to manage you expenses',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white, fontSize: 16),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            //Our MaterialButton which when pressed will take us to a new screen named as
+            //LoginScreen
+            MaterialButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50)),
+              elevation: 0,
+              height: 50,
+              onPressed: () {},
+              color: logoPurple,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('Get Started',
+                      style: TextStyle(color: Colors.white, fontSize: 20))
+                ],
+              ),
+              textColor: Colors.white,
+            )
+          ],
+        ),
+      ),
+    ); // This trailing comma makes auto-formatting nicer for build methods;
+  }
+}
