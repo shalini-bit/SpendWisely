c98ae209c72692ddf1323348dd22a24cba829db9
Shalini <64901722+shalini-bit@users.noreply.github.com>
2021-09-30 13:55:42 +0530

    Add files via upload
    second commit

---
+++ SpendWisely/lib/loginscreen.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/signup.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+// ignore: use_key_in_widget_constructors
+class LoginScreen extends StatelessWidget {
+  final Color primaryColor = Color(0xff0f284b);
+  final Color secondaryColor = Color(0xff232c51);
+
+  final Color logoPurple = Color(0xff6a1b9a);
+
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        backgroundColor: primaryColor,
+        body: Container(
+          alignment: Alignment.topCenter,
+          margin: EdgeInsets.symmetric(horizontal: 30),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Sign in to Spend Wisely and continue',
+                  textAlign: TextAlign.center,
+                  style:
+                      GoogleFonts.openSans(color: Colors.white, fontSize: 28),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Enter your email and password below to continue to the Spend Wisely!',
+                  textAlign: TextAlign.center,
+                  style:
+                      GoogleFonts.openSans(color: Colors.white, fontSize: 14),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                _buildTextField(
+                    nameController, Icons.account_circle, 'Username'),
+                SizedBox(height: 20),
+                _buildTextField(passwordController, Icons.lock, 'Password'),
+                SizedBox(height: 30),
+                MaterialButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50)),
+                  elevation: 0,
+                  minWidth: double.maxFinite,
+                  height: 50,
+                  onPressed: () {},
+                  color: logoPurple,
+                  child: Text('Sign-in',
+                      style: TextStyle(color: Colors.white, fontSize: 16)),
+                  textColor: Colors.white,
+                ),
+                SizedBox(height: 20),
+                MaterialButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50)),
+                  elevation: 0,
+                  minWidth: double.maxFinite,
+                  height: 50,
+                  onPressed: () {
+                    //Here goes the logic for Google SignIn discussed in the next section
+                  },
+                  color: Colors.blue,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(FontAwesomeIcons.google),
+                      SizedBox(width: 10),
+                      Text('Sign-in using Google',
+                          style: TextStyle(color: Colors.white, fontSize: 16)),
+                    ],
+                  ),
+                  textColor: Colors.white,
+                ),
+                SizedBox(height: 100),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Don't have an account ? ",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (_) => SignUpPage()));
+                        },
+                        child: Text(
+                          'Sign up',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+
+  _buildTextField(
+      TextEditingController controller, IconData icon, String labelText) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      decoration: BoxDecoration(
+          color: secondaryColor, border: Border.all(color: Colors.blue)),
+      child: TextField(
+        controller: controller,
+        style: TextStyle(color: Colors.white),
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(horizontal: 10),
+            labelText: labelText,
+            labelStyle: TextStyle(color: Colors.white),
+            icon: Icon(
+              icon,
+              color: Colors.white,
+            ),
+            // prefix: Icon(icon),
+            border: InputBorder.none),
+      ),
+    );
+  }
+}
---
+++ SpendWisely/lib/signup.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/loginscreen.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class SignUpPage extends StatefulWidget {
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  Color primaryColor = Color(0xff0f284b);
+  Color secondaryColor = Color(0xff232c51);
+  Color logoPurple = Color(0xff6a1b9a);
+
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController ConfirmpasswordController =
+      TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset: true,
+        backgroundColor: primaryColor,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: Container(
+          alignment: Alignment.topCenter,
+          margin: EdgeInsets.symmetric(horizontal: 40),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Sign Up',
+                  textAlign: TextAlign.center,
+                  style:
+                      GoogleFonts.openSans(color: Colors.white, fontSize: 28),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Create an account ,Its free',
+                  textAlign: TextAlign.center,
+                  style:
+                      GoogleFonts.openSans(color: Colors.white, fontSize: 14),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                _buildTextField(
+                    nameController, Icons.account_circle, 'Username'),
+                SizedBox(height: 20),
+                _buildTextField(emailController, Icons.lock, 'Email'),
+                SizedBox(height: 20),
+                _buildTextField(passwordController, Icons.lock, 'Password'),
+                SizedBox(height: 20),
+                _buildTextField(
+                    ConfirmpasswordController, Icons.lock, 'Confirm Password'),
+                SizedBox(height: 20),
+                MaterialButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50)),
+                  elevation: 0,
+                  minWidth: double.maxFinite,
+                  height: 50,
+                  onPressed: () {},
+                  color: logoPurple,
+                  child: Text('Sign up',
+                      style: TextStyle(color: Colors.white, fontSize: 16)),
+                  textColor: Colors.white,
+                ),
+                SizedBox(height: 20),
+                MaterialButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50)),
+                  elevation: 0,
+                  minWidth: double.maxFinite,
+                  height: 50,
+                  onPressed: () {
+                    //Here goes the logic for Google SignIn discussed in the next section
+                  },
+                  color: Colors.blue,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(FontAwesomeIcons.google),
+                      SizedBox(width: 10),
+                      Text('Sign-up using Google',
+                          style: TextStyle(color: Colors.white, fontSize: 16)),
+                    ],
+                  ),
+                  textColor: Colors.white,
+                ),
+                SizedBox(height: 50),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Already have an account ?",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (_) => LoginScreen()));
+                        },
+                        child: Text(
+                          'Sign in',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+
+  _buildTextField(
+      TextEditingController controller, IconData icon, String labelText) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      decoration: BoxDecoration(
+          color: secondaryColor, border: Border.all(color: Colors.blue)),
+      child: TextField(
+        controller: controller,
+        style: TextStyle(color: Colors.white),
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(horizontal: 10),
+            labelText: labelText,
+            labelStyle: TextStyle(color: Colors.white),
+            icon: Icon(
+              icon,
+              color: Colors.white,
+            ),
+            // prefix: Icon(icon),
+            border: InputBorder.none),
+      ),
+    );
+  }
+}
